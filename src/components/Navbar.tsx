5acc49f93c43a810f5253c3ffed38a844e946e6f
k4ta-dev <samflynn19121@gmail.com>
2025-10-30 14:05:07 +0400

    fix:use link instead of href to avoid flashes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,24 @@
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav className="w-full max-w-[980px] mx-auto mt-5 flex items-center justify-center px-6 py-3 bg-foreground border-b-2 brand-border">
       <div className="flex gap-4 text-sm">
-        <a href="/home" className="button-like">
+        <Link to="/home" className="button-like">
           [ Home ]
-        </a>
+        </Link>
 
-        <a href="/whoami" className="button-like">
+        <Link to="/whoami" className="button-like">
           [ WHOAMI ]
-        </a>
+        </Link>
 
-        <a href="/contact" className="button-like">
+        <Link to="/contact" className="button-like">
           [ Contact ]
-        </a>
+        </Link>
 
-        <a href="/blog" className="button-like">
+        <Link to="/blog" className="button-like">
           [ Blog ]
-        </a>
+        </Link>
 
       </div>
     </nav>
